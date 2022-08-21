44a61503865bef6c8279a9487f832686be9b6e5f
Vinicius Braz <vinicius_lsb@live.com>
2022-08-21 00:16:57 -0300

    implements Item s crud

--- src/main/java/com/godevsenior/orderclosingapi/controllers/ItemController.java
+++ src/main/java/com/godevsenior/orderclosingapi/controllers/ItemController.java
@@ -4,11 +4,10 @@ import com.godevsenior.orderclosingapi.dto.ItemDTO;
 import com.godevsenior.orderclosingapi.services.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,18 +16,40 @@ public class ItemController {
 
     @Autowired
     private ItemService service;
-
     @GetMapping
-    public ResponseEntity<List<ItemDTO>>findAll() {
+    public ResponseEntity<List<ItemDTO>> findAll() {
         List<ItemDTO> itemsList = service.findAll();
 
         return ResponseEntity.ok().body(itemsList);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<ItemDTO>findById(@PathVariable Long id) {
+    public ResponseEntity<ItemDTO> findById(@PathVariable Long id) {
         ItemDTO dto = service.findById(id);
 
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ItemDTO> insert(@RequestBody ItemDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ItemDTO> update(@PathVariable Long id, @RequestBody ItemDTO dto) {
+        dto = service.update(id, dto);
+
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<ItemDTO> delete(@PathVariable Long id) {
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/godevsenior/orderclosingapi/services/ItemService.java
+++ src/main/java/com/godevsenior/orderclosingapi/services/ItemService.java
@@ -3,11 +3,15 @@ package com.godevsenior.orderclosingapi.services;
 import com.godevsenior.orderclosingapi.dto.ItemDTO;
 import com.godevsenior.orderclosingapi.entities.Item;
 import com.godevsenior.orderclosingapi.repositories.ItemRepository;
+import com.godevsenior.orderclosingapi.services.exceptions.DatabaseException;
 import com.godevsenior.orderclosingapi.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,5 +34,40 @@ public class ItemService {
         Item entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 
         return new ItemDTO(entity);
+    }
+
+    @Transactional
+    public ItemDTO insert(ItemDTO dto) {
+        Item entity = new Item();
+        entity.setDescription(dto.getDescription());
+        entity.setCost(dto.getCost());
+        entity.setType(dto.getType());
+        entity = repository.save(entity);
+
+        return new ItemDTO(entity);
+    }
+
+    @Transactional
+    public ItemDTO update(Long id, ItemDTO dto) {
+        try {
+            Item entity = repository.getReferenceById(id);
+            entity.setDescription(dto.getDescription());
+            entity.setCost(dto.getCost());
+            entity.setType(dto.getType());
+            entity = repository.save(entity);
+            return new ItemDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found: " + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found: " + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
     }
 }
