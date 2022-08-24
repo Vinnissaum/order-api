9aa510c1ba1f0df44419540edf86b7818919907d
Vinicius Braz <vinicius_lsb@live.com>
2022-08-24 10:55:53 -0300

    feat: postgres connection

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
-INSERT INTO tb_item (DESCRIPTION, COST, TYPE) VALUES ('Apartment 3 rooms', 500000.00, 'P');
-INSERT INTO tb_item (DESCRIPTION, COST, TYPE) VALUES ('Sanitation', 1000.00, 'S');
-
-INSERT INTO tb_order (NUMBER, DATE, percentage_Discount) VALUES (25, NOW(), 0.2);
-INSERT INTO tb_order (NUMBER, DATE, percentage_Discount) VALUES (100, NOW(), 0.5);
-
-INSERT INTO tb_order_item (item_Id, order_Id, QUANTITY) VALUES (1, 1, 5);
-INSERT INTO tb_order_item (item_Id, order_Id, QUANTITY) VALUES (2, 1, 10);
+--INSERT INTO tb_item (DESCRIPTION, COST, TYPE) VALUES ('Apartment 3 rooms', 500000.00, 'P');
+--INSERT INTO tb_item (DESCRIPTION, COST, TYPE) VALUES ('Sanitation', 1000.00, 'S');
+--
+--INSERT INTO tb_order (NUMBER, DATE, percentage_Discount) VALUES (25, NOW(), 0.2);
+--INSERT INTO tb_order (NUMBER, DATE, percentage_Discount) VALUES (100, NOW(), 0.5);
+--
+--INSERT INTO tb_order_item (item_Id, order_Id, QUANTITY) VALUES (1, 1, 5);
+--INSERT INTO tb_order_item (item_Id, order_Id, QUANTITY) VALUES (2, 1, 10);
